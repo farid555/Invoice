d368081c8092510440efcb74faf57975bedef530
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-07-17 18:46:23 +0600

    chore: add deps and mongodb service connection

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,38 @@
-import chalk from "chalk"
-import cookieParser from "cookie-parser"
-import "dotenv/config"
-import express from "express"
-import morgan from "morgan"
-import {morganMiddleware, systemLogs} from "./utils/Logger.js"
+import chalk from 'chalk';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+import connectionToDB from './config/connectDB.js';
+import { morganMiddleware, systemLogs } from './utils/Logger.js';
 
-const app = express()
+await connectionToDB();
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan("dev"))
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
-app.use(morganMiddleware)
+app.use(cookieParser());
+app.use(morganMiddleware);
 
-app.get("/api/v1/test", (req, res)=>{
-    res.json({Hi: "Welcome to the Invoice App"})
-})
+app.get('/api/v1/test', (req, res) => {
+  res.json({ Hi: 'Welcome to the Invoice App' });
+});
 
-const PORT = process.env.PORT || 1997
+const PORT = process.env.PORT || 1997;
 
-
-app.listen(PORT, ()=>{
-    console.log(`${chalk.green.bold("✔")} 👍 Server running is ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port ${chalk.green.bold(PORT)}...`);
-    systemLogs.info(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
-})
+app.listen(PORT, () => {
+  console.log(
+    `${chalk.green.bold('✔')} 👍 Server running is ${chalk.yellow.bold(
+      process.env.NODE_ENV
+    )} mode on port ${chalk.green.bold(PORT)}...`
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the invoice App...</h1>
+      <h1>Welcome to the invoice App...> Hello there!</h1>
     </div>
   )
 }
