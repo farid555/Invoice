ff1b35d655a31c61721f2b087f281e11a8a04003
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-08-18 22:38:34 +0300

    delete my account

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import getUserProfile from '../controllers/user/getUserProfile.js';
 import updateUserProfile from '../controllers/user/updateUserProfile.js';
+import deleteMyAccount from '../controllers/user/deleteMyAccount.js';
 import checkAuth from '../middleware/checkAuthMiddleware.js';
 
 const router = express.Router();
@@ -8,6 +9,7 @@ const router = express.Router();
 router
   .route('/profile')
   .get(checkAuth, getUserProfile)
-  .patch(checkAuth, updateUserProfile);
+  .patch(checkAuth, updateUserProfile)
+  .delete(checkAuth, deleteMyAccount);
 
 export default router;
