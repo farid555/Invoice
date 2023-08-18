c4dcee69428e630b47e71bb2fcfff5cb937c7482
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-08-18 20:14:48 +0300

    user profile and user update

---
+++ backend/controllers/user/updateUserProfile.js
@@ -0,0 +1,55 @@
+import asyncHandler from 'express-async-handler';
+import User from '../../models/userModel.js';
+
+//$-title  Update User Profile
+
+//$-path   GET /api/v1/user/profile
+//$-auth   private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const {
+    password,
+    passwordConfirm,
+    email,
+    isEmailVerified,
+    provider,
+    roles,
+    googleID,
+    username,
+  } = req.body;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    res.status(400);
+    throw new Error('That user does not exist in our system');
+  }
+
+  if (password || passwordConfirm) {
+    res.status(400);
+    throw new Error('This route is not for passwprd updates,');
+  }
+
+  if (email || isEmailVerified || provider || roles || googleID) {
+    res.status(400);
+    throw new Error('You are not allowed to update that field on this route');
+  }
+
+  const fieldsToUpdate = req.body;
+
+  const updatedProfile = await User.findByIdAndUpdate(
+    userId,
+    { ...fieldsToUpdate },
+    { new: true, runValidators: true }
+  ).select('-refreshToken');
+
+  res.status(200).json({
+    success: true,
+    message: `${user.firstName}, your profile was successfully updated`,
+    updatedProfile,
+  });
+});
+
+export default updateUserProfile;
