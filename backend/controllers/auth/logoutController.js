b62a92bad4515f33b08ad463144261fc63fa721c
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-08-18 12:30:11 +0300

    feat: Logout User and refresh cookies

---
+++ backend/controllers/auth/logoutController.js
@@ -0,0 +1,46 @@
+import asyncHandler from 'express-async-handler';
+import User from '../../models/userModel.js';
+import { systemLogs } from '../../utils/Logger.js';
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const cookies = req.cookies;
+
+  systemLogs.error(`${cookies}`);
+
+  if (!cookies?.jwt) {
+    res.sendStatus(204);
+    throw new Error('No cookie found');
+  }
+
+  const refreshToken = cookies.jwt;
+
+  const existingUser = await User.findOne({ refreshToken });
+
+  if (!existingUser) {
+    res.clearCookie('jwt', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'None',
+    });
+
+    res.sendStatus(204);
+  }
+
+  existingUser.refreshToken = existingUser.refreshToken.filter(
+    (reft) => reft != refreshToken
+  );
+  await existingUser.save();
+
+  res.clearCookie('jwt', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None',
+  });
+
+  res.status(200).json({
+    success: true,
+    message: `${existingUser.firstName}, you have been logged out successfully.`,
+  });
+});
+
+export default logoutUser;
