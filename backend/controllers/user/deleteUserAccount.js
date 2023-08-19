f9e18d0bb924e0711dff3fa16def6a4c35ac6877
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-08-19 08:08:22 +0300

    Delete User account (Admin)

---
+++ backend/controllers/user/deleteUserAccount.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler';
+import User from '../../models/userModel.js';
+import { systemLogs } from '../../utils/Logger.js';
+
+//$-title  Delete User Account
+//$-path   DELETE /api/v1/user/:id
+//$-auth   Private/Admin
+// an admin user can delete any other user account
+
+const deleteUserAccount = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    const result = await user.deleteOne();
+
+    res.json({
+      success: true,
+      message: `User ${result.firstName} deleted successsfully`,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+export default deleteUserAccount;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import deleteMyAccount from '../controllers/user/deleteMyAccount.js';
 import getAllUserAccounts from '../controllers/user/getAllUserAccount.js';
 import checkAuth from '../middleware/checkAuthMiddleware.js';
 import role from '../middleware/roleMiddleware.js';
+import deleteUserAccount from '../controllers/user/deleteUserAccount.js';
 
 const router = express.Router();
 
@@ -17,5 +18,9 @@ router
 router
   .route('/all')
   .get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
+
+router
+  .route('/:id')
+  .delete(checkAuth, role.checkRole(role.ROLES.Admin), deleteUserAccount);
 
 export default router;
