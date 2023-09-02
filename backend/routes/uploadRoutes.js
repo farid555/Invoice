9f1211ccce1288a356fa65aa16af58b329dc228a
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-09-03 00:20:12 +0300

    feat: upload image using cloudinary

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cloudinaryUploader from '../config/cloudinaryConfig.js';
+import upload from '../helpers/multer.js';
+import path from 'path';
+
+const router = express.Router();
+
+router.route('/').patch(upload.single('logo'), async (req, res) => {
+  const localFilePath = req.file.path;
+  const result = await cloudinaryUploader(localFilePath);
+
+  res.send(result.url);
+});
+
+export default router;
