e37304c191108aac214daec033e39c376fcb8afa
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-09-03 14:25:44 +0300

    get, update, delete user profile info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,8 @@ import { ROLES } from './config/roles';
 import UsersList from './features/users/pages/UsersListPage';
 import DashboardPage from './pages/DashboardPage';
 import AuthRequired from './components/AuthRequired';
+import EditProfileForm from './features/users/pages/EditProfileForm';
+import ProfilePage from './features/users/pages/ProfilePage';
 
 const App = () => {
   useTitle('MERN Invoice - Home');
@@ -48,6 +50,8 @@ const App = () => {
 
         <Route element={<AuthRequired allowedRoles={[ROLES.User]} />}>
           <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="edit-profile" element={<EditProfileForm />} />
         </Route>
 
         {/*Private Route - Admin Users only*/}
